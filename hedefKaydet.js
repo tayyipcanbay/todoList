aeadd82f858a6550b91a81067e84dd49e68965ea
tayyipcanbay <202013709006@ogr.balikesir.edu.tr>
2022-05-21 18:50:17 +0300

    a

--- hedefKaydet.js
+++ hedefKaydet.js
@@ -1,7 +1,7 @@
 var baglanti = require('./baglanti');
 function hedefKaydet() {
     let baslik = document.getElementById("baslik").value;
-    let aciklama = document.getElementById("editor").innerHTML;
+    let aciklama = document.getElementById("editor").value;
     let sonTarih = document.getElementById("sonTarih").value;
     baglanti.con.query("INSERT INTO `targetlist` (`id`, `header`, `text`, `creatingTime`, `lastTime`, `isDone`) VALUES (NULL, '" + baslik + "', '" + aciklama + "', current_timestamp(), '" + sonTarih + "', '0');", (err) => {
         if (err) throw err;
